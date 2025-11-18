b8c28d994d78f08edc76fe98ef6bc90b622b22ec
Joao de Deus <joaodeus400@gmail.com>
2025-11-18 16:22:32 -0300

    [MANUAL] Add DbContext in Path Data/Repository

---
+++ ChatIARag.API/Data/Context/ChatIARagDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ChatIARag.API.Config;
+using ChatIARag.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatIARag.API.Data.Context;
+
+public class ChatIARagDbContext: DbContext
+{
+    public ChatIARagDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+
+    public DbSet<Conversation> Conversation { get; set; }    
+    public DbSet<Message> Message { get; set; }
+    public DbSet<Document> Document { get; set; }
+    public DbSet<User> User { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ConversationConfiguration());
+        modelBuilder.ApplyConfiguration(new DocumentConfiguration());
+        modelBuilder.ApplyConfiguration(new MessageConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+    }
+}
---
+++ ChatIARag.API/Data/Context/ChatIARagDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ChatIARag.API.Data.Context;
+
+public class ChatIARagDbContextFactory : IDesignTimeDbContextFactory<ChatIARagDbContext>
+{
+    public ChatIARagDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.Development.json").Build();
+
+        var builder = new DbContextOptionsBuilder<ChatIARagDbContext>();
+
+        var connectionString = configuration.GetConnectionString("Connection");
+
+        builder.UseNpgsql(connectionString);
+
+        return new ChatIARagDbContext(builder.Options);
+    }
+}
