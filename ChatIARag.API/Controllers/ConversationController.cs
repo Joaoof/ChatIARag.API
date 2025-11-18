56fbe91abad8411d90350319106f4ec70fa1a839
Joao de Deus <joaodeus400@gmail.com>
2025-11-18 09:41:28 -0300

    Adicionar arquivos de projeto.

---
+++ ChatIARag.API/Controllers/ConversationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatIARag.API.Controllers
+{
+    public class ConversationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
