75befa5c48deba38a32ae0afa733622221d34675
Joao de Deus <joaodeus400@gmail.com>
2025-11-24 09:53:04 -0300

    [MANUAL] Create IUserRepository interface

---
+++ ChatIARag.API/Interface/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ChatIARag.API.Models;
+
+namespace ChatIARag.API.Interface.Repository;
+
+public interface IUserRepository
+{
+    ValueTask<User> GetByIdAsync(Guid id);
+    ValueTask<bool> IsEmailAvailableAsync(string email);
+    Task<User> GetByEmailAsync(string email);
+    Task CreateAsync (User user);
+    Task UpdateAsync (User user);
+
+}
