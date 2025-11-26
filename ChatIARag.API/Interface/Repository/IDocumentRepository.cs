918930518f41e75ee00c457b6645e9b7cbd589a0
Joao de Deus <joaodeus400@gmail.com>
2025-11-26 14:57:52 -0300

    [MANUAL] Create IDocumentRepository interface

---
+++ ChatIARag.API/Interface/Repository/IDocumentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata;
+
+namespace ChatIARag.API.Interface.Repository;
+
+public interface IDocumentRepository
+{
+    Task<List<Document>> GetAllAsync();
+    Task<Document?> GetByIdAsync(Guid id);
+    Task CreateAsync(Document document);
+    Task<Document?> UpdateAsync(Document document);
+    Task DeleteAsync(Guid id);
+}
